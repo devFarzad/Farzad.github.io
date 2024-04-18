aa893f17e638ddc5626608e9613fa281ecb6fa69
Farzad Ghader <farzad@datacode.app>
2024-04-18 19:32:47 +0300

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,14 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    trailingSlash: true,
+};
 
-// export default nextConfig;
+export default nextConfig;
 // const isProd = process.env.NODE_ENV === 'production';
 
-export default {
-
-    trailingSlash: true,
-  env: {
-    customKey: process.env.CUSTOM_KEY,
-  },
-};
+// export default {
+//     output: 'export',
+//     trailingSlash: true,
+//   };
+  
