90df6ca81eab0548d622c8ea80aa6b109860c1bb
Farzad Ghader <farzad@datacode.app>
2024-04-18 21:23:14 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -1,17 +1,11 @@
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-    output: 'export',
-   
-    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-    // trailingSlash: true,
-   
-    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-    // skipTrailingSlashRedirect: true,
-   
-    // Optional: Change the output directory `out` -> `dist`
-    // distDir: 'dist',
-  }
-   
-  module.exports = nextConfig
+module.exports = {
+    // Use the assetPrefix setting if your site is hosted on GitHub Pages in a repository
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/farzad.github.io/' : '',
+  
+    // Ensures that files are exported correctly for GitHub Pages
+    trailingSlash: true,
+    output: 'standalone',
+  
+    // Other configurations...
+  };
+  
