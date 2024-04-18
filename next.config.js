eb5002295049d35d0ba03e6d25587d79bdb2dbfe
Farzad Ghader <farzad@datacode.app>
2024-04-18 21:30:24 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     // Use the assetPrefix setting if your site is hosted on GitHub Pages in a repository
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/farzad.github.io/' : '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/' : '',
   
     // Ensures that files are exported correctly for GitHub Pages
     trailingSlash: true,
